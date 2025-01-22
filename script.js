e61ea9b3deda05d359b0e5d82109cbf987ffc33a
Eduardo220898 <eduardozollner@gmail.com>
2025-01-21 23:34:50 -0300

    Projeto Tesla

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let formulario = document.querySelector(".formulario")
+
+function aparecerformulario(){
+    formulario.style.left = "60%"
+
+}
+
+function desaparecerformulario(){
+    formulario.style.left = "-50%"
+
+}
